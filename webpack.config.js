f819c4140c152d1cd7ffda346721f6267b0af5e1
Makushev Oleg <makushev.o.k@yandex.ru>
2018-12-09 18:27:02 +0300

    Update for `webpack.config` and for `eslint` and `prettier`

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
-const resolve = require('path').resolve;
+const { resolve } = require('path');
 const webpack = require('webpack');
 
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin  = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = (env = {}, options = {}) => {
   const devMode = options.mode !== 'production';
-  
+  const sourceMap = !devMode;
+
   /* plagins setup start */
   const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
+    template: './src/index.html',
+    filename: './index.html'
   });
-  
+
   const cssPlugin = new MiniCssExtractPlugin({
     // Options similar to the same options in webpackOptions.output
     // both options are optional
     filename: devMode ? '[name].css' : '[name].[hash].css',
-    chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
   });
-  
+
   const optimizeCSSAssetsPlugin = new OptimizeCSSAssetsPlugin({});
-  
+
   const uglifyJsPlugin = new UglifyJsPlugin({
     uglifyOptions: {
-        compress: {
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-        },
-        mangle: true,
-        output: {
-            comments: false,
-        },
+      compress: {
+        warnings: false,
+        // Disabled because of an issue with Uglify breaking seemingly valid code:
+        // https://github.com/facebook/create-react-app/issues/2376
+        // Pending further investigation:
+        // https://github.com/mishoo/UglifyJS2/issues/2011
+        comparisons: false
+      },
+      mangle: true,
+      output: {
+        comments: false
+      }
     },
     cache: true,
     parallel: true,
-    sourceMap:  devMode ? true : false,
+    sourceMap
   });
 
   const webpackHMRPlugin = new webpack.HotModuleReplacementPlugin();
@@ -49,18 +50,13 @@ module.exports = (env = {}, options = {}) => {
 
   return {
     entry: {
-      app: [
-        `./src/index.js`,
-      ],
+      app: [`./src/index.js`]
     },
     output: {
-      path: resolve(__dirname, 'dist'),
+      path: resolve(__dirname, 'dist')
     },
     optimization: {
-      minimizer: [
-        uglifyJsPlugin,
-        optimizeCSSAssetsPlugin,
-      ],
+      minimizer: [uglifyJsPlugin, optimizeCSSAssetsPlugin]
     },
     module: {
       rules: [
@@ -68,31 +64,27 @@ module.exports = (env = {}, options = {}) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
-          },
+            loader: 'babel-loader'
+          }
         },
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
             devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-            "css-loader",
+            'css-loader',
             'postcss-loader',
-            "sass-loader"
+            'sass-loader'
           ]
-        },
+        }
       ]
     },
-    plugins: [
-      htmlPlugin,
-      cssPlugin,
-      webpackHMRPlugin,
-    ],
+    plugins: [htmlPlugin, cssPlugin, webpackHMRPlugin],
     devServer: {
       // hot: true,
       open: 'Chrome',
       index: 'index.html',
       port: env.port || 6969,
-      watchContentBase: true,
-    },
+      watchContentBase: true
+    }
   };
-}
+};
