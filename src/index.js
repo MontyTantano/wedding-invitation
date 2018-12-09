0c900226040f0108b2d5ceecb0903570b2a4d99c
Makushev Oleg <makushev.o.k@yandex.ru>
2018-12-09 18:27:26 +0300

    Added dist files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
 
-import style from "./index.scss";
+import style from './index.scss';
 
-const Index = () => {
-  return <div className='hellow-world'>Hello World!</div>;
-};
+const Index = () => <div className="hellow-world">Hello World!</div>;
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<Index />, document.getElementById('index'));
 
-export default hot(module)(App)
+export default hot(module)(App);
