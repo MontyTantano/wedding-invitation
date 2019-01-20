31538e2e4e84a0dc8a2816456126c3a58b1e6a9a
Makushev Oleg <makushev.o.k@yandex.ru>
2019-01-20 21:34:41 +0300

    change text position

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,8 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="wedding-app">
-        <GreetingPhoto />
         <GreetingText />
+        <GreetingPhoto />
       </div>
     );
   }
