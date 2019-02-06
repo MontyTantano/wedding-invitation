03a0c583a9cedf09b2611095cd21436c8d8c3ea4
Monty <makushev.o.k@yandex.ru>
2019-02-06 16:41:34 +0300

    added countdown to preloaded state

--- src/server/index.js
+++ src/server/index.js
@@ -4,13 +4,20 @@ import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import configureStore from '../configureStore';
 import App from '../containers/App';
+import Constants from '../constants/texts';
+import { calculateCountdown } from '../utils';
 import renderFullPage from './renderFullPage';
 
 const PORT = process.env.PORT || 5000;
 const app = Express();
 
 function handleRender(req, res) {
-  const store = configureStore();
+  const countdown = calculateCountdown(Constants.EVENT_DATE);
+  const store = configureStore({
+    ...(countdown ? {
+      countdown,
+    } : {}),
+  });
   const html = renderToString(
     <Provider store={store}>
       <App />
