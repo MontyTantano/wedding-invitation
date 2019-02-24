bdc3ed22ffcaa9fa723a1fa4676b3902b6736d8c
Makushev Oleg <makushev.o.k@yandex.ru>
2019-02-24 17:34:15 +0300

    add new email format

--- src/server/mailTemplate.js
+++ src/server/mailTemplate.js
@@ -1,9 +1,11 @@
 export default function template(formData = {}) {
   const { items } = formData;
-  const itemsText = (items || [])
-    .map(item => `${item.fio} (${item.isAdult ? 'Взрослый' : 'Ребенок'})`)
-    .join(', ');
-  return `Подтверждение от ${formData.fio}${
-    itemsText ? ', гости в сопровождении: ' : '.'
-  }${itemsText}.`;
+  const guestsTexts = [`1 - ${formData.fio}`];
+  (items || []).forEach((item, ind) => {
+    const itemText = `${item.fio} (${item.isAdult ? 'Взрослый' : 'Ребенок'})`;
+    guestsTexts.push(`${ind + 2} - ${itemText}`);
+  });
+  const guestsText = guestsTexts.join(' \n ');
+
+  return `Общее число гостей ${1 + items.length}: \n ${guestsText}`;
 }
