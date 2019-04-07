1cfcdad1faaecdabeaca11567c5e7031ca39c773
Makushev Oleg <makushev.o.k@yandex.ru>
2019-04-07 21:02:06 +0300

    added new items to guests dictionary

--- src/server/guests-dictionary.js
+++ src/server/guests-dictionary.js
@@ -1,25 +1,4 @@
 export const Guests = {
-  test: {
-    invitationText: 'test',
-    form: {
-      isReceived: false,
-      isSended: false,
-      isValid: true,
-      fio: 'test_fio',
-      items: [
-        {
-          id: '1',
-          fio: 'test_fio_item',
-          isAdult: true
-        },
-        {
-          id: '2',
-          fio: 'test_fio_item 2',
-          isAdult: false
-        }
-      ]
-    }
-  },
   karpova: {
     invitationText: 'Дорогая Лена!',
     form: {
@@ -28,6 +7,873 @@ export const Guests = {
       isValid: true,
       fio: 'Елена Карпова',
       items: []
+    }
+  },
+  family: {
+    invitationText: 'Дорогая семья!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Данилова Лидия',
+      items: [
+        {
+          fio: 'Данилов Валерий',
+          isAdult: true,
+          id: '1'
+        },
+        {
+          fio: 'Данилова Светлана',
+          isAdult: true,
+          id: '2'
+        },
+        {
+          fio: 'Данилов Андрей',
+          isAdult: true,
+          id: '3'
+        },
+        {
+          fio: 'Данилова Ксения',
+          isAdult: false,
+          id: '4'
+        },
+        {
+          fio: 'Данилов Иван',
+          isAdult: false,
+          id: '5'
+        }
+      ]
+    }
+  },
+  olya: {
+    invitationText: 'Дорогая тётушка!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Данилова Ольга',
+      items: []
+    }
+  },
+  danilovi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Данилов Михаил',
+      items: [
+        {
+          fio: 'Данилова Яна',
+          isAdult: true,
+          id: '6'
+        },
+        {
+          fio: 'Данилова Дарья',
+          isAdult: false,
+          id: '7'
+        }
+      ]
+    }
+  },
+  efremovi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Ефремова Наталья',
+      items: [
+        {
+          fio: 'Ефремова Ирина',
+          isAdult: true,
+          id: '8'
+        },
+        {
+          fio: 'Ефремов Юрий',
+          isAdult: true,
+          id: '9'
+        }
+      ]
+    }
+  },
+  kamnevi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Камнев Сергей',
+      items: [
+        {
+          fio: 'Камнева Елена',
+          isAdult: true,
+          id: '10'
+        },
+        {
+          fio: 'Камнева Александра',
+          isAdult: true,
+          id: '11'
+        },
+        {
+          fio: 'Камнева Полина',
+          isAdult: true,
+          id: '12'
+        },
+        {
+          fio: 'Камнева Анастасия',
+          isAdult: true,
+          id: '13'
+        }
+      ]
+    }
+  },
+  sonyatimur: {
+    invitationText: 'Дорогие Соня и Тимур!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Камнева Софья',
+      items: [
+        {
+          fio: 'Бапбыев Тимур',
+          isAdult: true,
+          id: '14'
+        }
+      ]
+    }
+  },
+  anyakamneva: {
+    invitationText: 'Дорогая сестрёнка!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Камнева Анна',
+      items: []
+    }
+  },
+  repini: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Репина Елена',
+      items: [
+        {
+          fio: 'Репин Владимир',
+          isAdult: true,
+          id: '15'
+        },
+        {
+          fio: 'Репина Виктория',
+          isAdult: true,
+          id: '16'
+        },
+        {
+          fio: 'Репина Елизавета',
+          isAdult: true,
+          id: '17'
+        },
+        {
+          fio: 'Репин Игорь',
+          isAdult: true,
+          id: '18'
+        }
+      ]
+    }
+  },
+  filimonovi: {
+    invitationText: 'Дорогие Наташа и Катюша!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Филимонова Наталья',
+      items: [
+        {
+          fio: 'Филимонова Екатерина',
+          isAdult: false,
+          id: '19'
+        }
+      ]
+    }
+  },
+  ritazhenya: {
+    invitationText: 'Дорогие Маргарита и Евгений!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Чигрина Маргарита',
+      items: [
+        {
+          fio: 'Чигрин Евгений',
+          isAdult: true,
+          id: '20'
+        }
+      ]
+    }
+  },
+  kymi: {
+    invitationText: 'Дорогие кумовья!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Максимова Инна',
+      items: [
+        {
+          fio: 'Дорохов Денис',
+          isAdult: true,
+          id: '21'
+        },
+        {
+          fio: 'Максимов Максим',
+          isAdult: false,
+          id: '22'
+        }
+      ]
+    }
+  },
+  rizhenko: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Рыженко Сергей',
+      items: [
+        {
+          fio: 'Рыженко Алла',
+          isAdult: true,
+          id: '23'
+        },
+        {
+          fio: 'Рыженко Анастасия',
+          isAdult: true,
+          id: '24'
+        },
+        {
+          fio: 'Рыженко Елизавета',
+          isAdult: true,
+          id: '25'
+        }
+      ]
+    }
+  },
+  tygolukovi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Туголуков Вадим',
+      items: [
+        {
+          fio: 'Тулогукова Елена',
+          isAdult: true,
+          id: '26'
+        },
+        {
+          fio: 'Туголуков Богдан',
+          isAdult: true,
+          id: '27'
+        },
+        {
+          fio: 'Туголуков Иван',
+          isAdult: false,
+          id: '28'
+        },
+        {
+          fio: 'Туголукова Елена',
+          isAdult: false,
+          id: '29'
+        }
+      ]
+    }
+  },
+  stolyarovi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Столяров Сергей',
+      items: [
+        {
+          fio: 'Столярова Екатерина',
+          isAdult: true,
+          id: '30'
+        }
+      ]
+    }
+  },
+  stolyarovim: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Столяров Александр',
+      items: [
+        {
+          fio: 'Столярова Марина',
+          isAdult: true,
+          id: '31'
+        }
+      ]
+    }
+  },
+  golybovi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Голубов Михаил',
+      items: [
+        {
+          fio: 'Голубова Алла',
+          isAdult: true,
+          id: '32'
+        },
+        {
+          fio: 'Голубов Мишутка',
+          isAdult: true,
+          id: '33'
+        }
+      ]
+    }
+  },
+  kovtun: {
+    invitationText: 'Дорогие Ковтуны!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Ковтун Олег',
+      items: [
+        {
+          fio: 'Ковтун Оксана',
+          isAdult: true,
+          id: '34'
+        },
+        {
+          fio: 'Ковтун Богдан',
+          isAdult: true,
+          id: '35'
+        },
+        {
+          fio: 'Ковтун Мирослав',
+          isAdult: true,
+          id: '36'
+        }
+      ]
+    }
+  },
+  anyavitalik: {
+    invitationText: 'Дорогие Анна и Виталик!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Вирбицкас Анна',
+      items: [
+        {
+          fio: 'Вирбицкас Виталий',
+          isAdult: true,
+          id: '37'
+        }
+      ]
+    }
+  },
+  darya: {
+    invitationText: 'Дорогая Дарья!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Бурунина Дарья',
+      items: []
+    }
+  },
+  lahno: {
+    invitationText: 'Дорогие Галя и Вадим!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Лахно Галина',
+      items: [
+        {
+          fio: 'Лахно Вадим',
+          isAdult: true,
+          id: '38'
+        },
+        {
+          fio: 'Лахно Мария',
+          isAdult: false,
+          id: '39'
+        }
+      ]
+    }
+  },
+  dasha: {
+    invitationText: 'Дорогая Даша!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Коваль Дарья',
+      items: []
+    }
+  },
+  inna: {
+    invitationText: 'Дорогая Инна!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Гречка Инна',
+      items: []
+    }
+  },
+  ediknina: {
+    invitationText: 'Дорогие Эдик и Нина!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Евдокимов Эдуард',
+      items: [
+        {
+          fio: 'Евдокимова Нина',
+          isAdult: true,
+          id: '40'
+        }
+      ]
+    }
+  },
+  roma: {
+    invitationText: 'Дорогой Рома',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Еремин Роман',
+      items: []
+    }
+  },
+  zhenyaolesia: {
+    invitationText: 'Дорогие Женя и Олеся!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Шурупов Евгений',
+      items: [
+        {
+          fio: 'Овдиенко Олеся',
+          isAdult: true,
+          id: '41'
+        }
+      ]
+    }
+  },
+  alexandr: {
+    invitationText: 'Дорогой Саша!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Стрельников Александр',
+      items: []
+    }
+  },
+  roditeli: {
+    invitationText: 'Дорогие родители!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Боговик Евгений',
+      items: [
+        {
+          fio: 'Макушева Татьяна',
+          isAdult: true,
+          id: '42'
+        }
+      ]
+    }
+  },
+  alekseevi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Алексеев Андрей',
+      items: [
+        {
+          fio: 'Алексеева Ирина',
+          isAdult: true,
+          id: '43'
+        },
+        {
+          fio: 'Алексеев Игорь',
+          isAdult: false,
+          id: '44'
+        }
+      ]
+    }
+  },
+  nefedovi: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Нефедов Константин',
+      items: [
+        {
+          fio: 'Нефедова Лариса',
+          isAdult: true,
+          id: '45'
+        }
+      ]
+    }
+  },
+  zhenya: {
+    invitationText: 'Дорогая сестра!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Минькова Евгения',
+      items: [
+        {
+          fio: 'Миньков Миша',
+          isAdult: true,
+          id: '46'
+        }
+      ]
+    }
+  },
+  dovdal: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Довгаль Иван',
+      items: [
+        {
+          fio: 'Довгаль Екатерина',
+          isAdult: true,
+          id: '47'
+        },
+        {
+          fio: 'Довгаль Егор',
+          isAdult: false,
+          id: '48'
+        }
+      ]
+    }
+  },
+  krestenchuk: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Крестенчук Валерий',
+      items: [
+        {
+          fio: 'Крестенчук Ольга',
+          isAdult: true,
+          id: '49'
+        }
+      ]
+    }
+  },
+  krestenchyk: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Крестенчук Денис',
+      items: [
+        {
+          fio: 'Крестенчук Олеся',
+          isAdult: true,
+          id: '50'
+        }
+      ]
+    }
+  },
+  kashinial: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Кашин Аркадий',
+      items: [
+        {
+          fio: 'Кашина Любовь',
+          isAdult: true,
+          id: '51'
+        }
+      ]
+    }
+  },
+  kashinivn: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Кашин Владимир',
+      items: [
+        {
+          fio: 'Кашина Наталья',
+          isAdult: true,
+          id: '52'
+        },
+        {
+          fio: 'Кашина Виктория',
+          isAdult: true,
+          id: '53'
+        },
+        {
+          fio: 'Кашин Егор',
+          isAdult: false,
+          id: '54'
+        },
+        {
+          fio: 'Кашина Людмила',
+          isAdult: false,
+          id: '55'
+        }
+      ]
+    }
+  },
+  kashiniea: {
+    invitationText: 'Дорогие родственники!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Кашин Евгений',
+      items: [
+        {
+          fio: 'Кашина Анна',
+          isAdult: true,
+          id: '56'
+        },
+        {
+          fio: 'Кашина Шарлотта',
+          isAdult: false,
+          id: '57'
+        },
+        {
+          fio: 'Кашина Анастасия',
+          isAdult: false,
+          id: '58'
+        }
+      ]
+    }
+  },
+  babushka: {
+    invitationText: 'Дорогая Бабушка!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Боговик Любовь',
+      items: []
+    }
+  },
+  burunin: {
+    invitationText: 'Дорогой Дима!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Бурунин Дмитрий',
+      items: []
+    }
+  },
+  burunina: {
+    invitationText: 'Дорогая Алёна!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Бурунина Алёна',
+      items: []
+    }
+  },
+  dimanastya: {
+    invitationText: 'Дорогой Дима и Настя!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Котляров Дмитрий',
+      items: [
+        {
+          fio: 'Анастасия Касьянова',
+          isAdult: true,
+          id: '59'
+        }
+      ]
+    }
+  },
+  slavania: {
+    invitationText: 'Дорогие Слава и Аня!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Дегтярев Вячеслав',
+      items: [
+        {
+          fio: 'Ломагина Анна',
+          isAdult: true,
+          id: '60'
+        }
+      ]
+    }
+  },
+  sasha: {
+    invitationText: '	Дорогие Саша и Аня!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Александр Калашников',
+      items: [
+        {
+          fio: 'Анна Щербинина',
+          isAdult: true,
+          id: '61'
+        }
+      ]
+    }
+  },
+  alex: {
+    invitationText: 'Дорогой Леша!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Алексей Дородин',
+      items: []
+    }
+  },
+  yura: {
+    invitationText: 'Дорогой Юра!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Сойка Юрий',
+      items: []
+    }
+  },
+  kovalskie: {
+    invitationText: 'Дорогие Владимир и Мария!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Ковальский Владимир',
+      items: [
+        {
+          fio: 'Ковальская Мария',
+          isAdult: true,
+          id: '62'
+        }
+      ]
+    }
+  },
+  shamal: {
+    invitationText: 'Дорогие Виталик и Таня!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Шамаль Виталик',
+      items: [
+        {
+          fio: 'Шамаль Татьяна',
+          isAdult: true,
+          id: '63'
+        }
+      ]
+    }
+  },
+  filatovioi: {
+    invitationText: 'Дорогие Оля и Игорь!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Филатова Ольга',
+      items: [
+        {
+          fio: 'Филатов Игорь',
+          isAdult: true,
+          id: '64'
+        }
+      ]
+    }
+  },
+  filatovaa: {
+    invitationText: 'Дорогая Аня!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Филатова Анна',
+      items: []
+    }
+  },
+  filatovimd: {
+    invitationText: 'Дорогие Маша и Данил!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Филатова Мария',
+      items: [
+        {
+          fio: 'Касторнов Данил',
+          isAdult: true,
+          id: '65'
+        }
+      ]
+    }
+  },
+  dmitrenko: {
+    invitationText: 'Дорогие Женя и Галя!',
+    form: {
+      isReceived: false,
+      isSended: false,
+      isValid: true,
+      fio: 'Дмитренко Евгений',
+      items: [
+        {
+          fio: 'Дмитренко Галина',
+          isAdult: true,
+          id: '66'
+        }
+      ]
     }
   }
 };
